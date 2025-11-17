515669cc7537d13561c72db13adb9e1249b6f81b
naylaqanitaalifia <naylaqanita286@gmail.com>
2025-11-17 14:30:57 +0700

    account-settings page done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/account_settings.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,8 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const AccountSettings(),
     );
   }
 }
